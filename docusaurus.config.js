5eef872be485d6cc0a1bbb52128e7353d32e0aa8
Matt Aitken <matt@mattaitken.com>
2022-09-09 14:43:53 +0100

    Update the URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,53 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'API Hero Docs',
-  tagline: 'Learn stuff',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "API Hero Docs",
+  tagline: "Learn stuff",
+  url: "https://docs.apihero.run",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'API Hero', // Usually your GitHub org/user name.
-  projectName: 'API Hero', // Usually your repo name.
+  organizationName: "API Hero", // Usually your GitHub org/user name.
+  projectName: "API Hero", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -57,57 +57,57 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'API Hero Docs',
+        title: "API Hero Docs",
         logo: {
-          alt: 'API Hero Docs',
-          src: 'img/logo.svg',
+          alt: "API Hero Docs",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'introduction',
-            position: 'left',
-            label: 'Documentation',
+            type: "doc",
+            docId: "introduction",
+            position: "left",
+            label: "Documentation",
           },
-         
+
           {
-            href: 'https://github.com/jsonhero-io/api-hero',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/jsonhero-io/api-hero",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/gzn9G8Sv',
+                label: "Discord",
+                href: "https://discord.gg/gzn9G8Sv",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/tryapihero',
+                label: "Twitter",
+                href: "https://twitter.com/tryapihero",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/jsonhero-io/api-hero',
+                label: "GitHub",
+                href: "https://github.com/jsonhero-io/api-hero",
               },
             ],
           },
