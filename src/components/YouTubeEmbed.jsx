2626695b292217d944ba7584c991861e0683e546
Matt Aitken <matt@mattaitken.com>
2022-09-15 16:42:59 +0100

    You can pass a width in to the YouTube player

--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./YouTubeEmbed.module.css";
 
-export function YouTubeEmbed({ embedId }) {
+const aspectRatio = 16 / 9;
+
+export function YouTubeEmbed({ embedId, width = 560 }) {
   return (
     <div className="video-responsive">
       <iframe
-        width="560"
-        height="315"
+        width={`${width}`}
+        height={`${width / aspectRatio}`}
         src={`https://www.youtube.com/embed/${embedId}`}
         title="YouTube video player"
         frameBorder="0"
